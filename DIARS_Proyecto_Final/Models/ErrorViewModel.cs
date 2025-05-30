ac3481a325d306a3747a6a035292436bedada75b
fabricioronaldogil@gmail.com <fabricioronaldogil@gmail.com>
2025-05-30 15:06:44 -0500

    Agregar archivos de proyecto.

---
+++ DIARS_Proyecto_Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DIARS_Proyecto_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
